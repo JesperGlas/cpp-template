f40010ad189c14b2466d88f3f3571d51d8405866
jesperglas <jesper.glas@gmail.com>
2022-04-13 21:31:11 +0200

    Progress template. Added comments.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,9 @@
 
 int main(int argc, char** argv)
 {
-    std::cout << "Hello world!" << std::endl;
+    std::cout   << "Starting " << PROJECT_NAME << "...\n"
+                << "Version: " << PROJECT_VERSION_MAJOR << "." << PROJECT_VERSION_MINOR
+                << std::endl;
 
     return 0;
 }
